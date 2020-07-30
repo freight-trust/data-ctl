3ed904f1b9d701ec0472aa6b705c356ddca112c4
sam <sam@freighttrust.com>
2020-07-30 07:46:38 -0700

    feat(bump): 0.2.1
    fixing some issues with py

--- drewry_container_index/tests/test_drewry_container_index.py
+++ drewry_container_index/tests/test_drewry_container_index.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env python
-
 """Tests for `drewry_container_index` package."""
 
-
 import unittest
+
 from click.testing import CliRunner
 
 from drewry_container_index import drewry_container_index
-from drewry_container_index import cli
 
+from drewry_container_index import cli
 
 class TestDrewry_container_index(unittest.TestCase):
     """Tests for `drewry_container_index` package."""
@@ -24,10 +22,17 @@ class TestDrewry_container_index(unittest.TestCase):
 
     def test_command_line_interface(self):
         """Test the CLI."""
+
         runner = CliRunner()
+
         result = runner.invoke(cli.main)
+
         assert result.exit_code == 0
+
         assert 'drewry_container_index.cli.main' in result.output
+
         help_result = runner.invoke(cli.main, ['--help'])
+
         assert help_result.exit_code == 0
+
         assert '--help  Show this message and exit.' in help_result.output
